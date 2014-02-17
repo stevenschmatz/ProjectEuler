8c1563b51235002297dc2d55acb002119bfa34d1
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-17 17:17:00 -0500

    added consecutive primes sum attempt

---
+++ Python/Consecutive_Prime_Sum_Attempt.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def primesfrom2to(n):
+    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = np.ones(n/3 + (n%6==2), dtype=np.bool)
+    sieve[0] = False
+    for i in xrange(int(n**0.5)/3+1):
+        if sieve[i]:
+            k=3*i+1|1
+            sieve[((k*k)/3)::2*k] = False
+            sieve[(k*k+4*k-2*k*(i&1))/3::2*k] = False
+    return np.r_[2,3,((3*np.nonzero(sieve)[0]+1)|1)]
+
+prime_list=primesfrom2to(10**7)
+
+def isprime(x):
+    count = 0
+    for prime in prime_list:
+        if x % prime == 0:
+            if count == 1:
+                return False
+            count += 1
+    return True
+
+print isprime(7)
