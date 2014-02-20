6d783e49b025216318443b8a3425e21ac28ddb15
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-20 18:27:02 -0500

    triangular solution made clearer

--- Python/Triangular_Pentagonal_Hexagonal.py
+++ Python/Triangular_Pentagonal_Hexagonal.py
@@ -1,19 +1,13 @@
 """
+Steven Schmatz
 								Expanded			Secondary diff. 	Tertiary diff.			
 Triangle numbers: n(n+1)/2 =	(n^2)/2 + n/2 		1, 2, 3, 4 ... 		1
 Pentagonal numbers: n(3n-1)/2 = 3(n^2)/2 - n/2 		1, 4, 7, 10... 		3
 Hexagonal numbers: n(2n-1) = 	2(n^2) - n 			1, 5, 9, 13... 		4
 
+Inverses of the functions are below.
+
 """
-
-def getTriangular(n):
-	return n*(n+1)/2
-
-def getPentagonal(n):
-	return n*(3*n-1)/2
-
-def getHexagonal(n):
-	return n*(2*n-1)
 
 def isTriangular(k):
 	n_value = 0.5*(-1+(8*k+1)**(0.5))
