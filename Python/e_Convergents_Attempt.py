af111733f44791072e87bf611b2f138e794f3eff
Steven Schmatz <stevenschmatz@gmail.com>
2014-02-27 22:20:41 -0500

    added sqrt 3 and sqrt 23 approximations

--- Python/e_Convergents_Attempt.py
+++ Python/e_Convergents_Attempt.py
@@ -1,6 +1,7 @@
 import math
 from fractions import Fraction
 
+
 def sqrt_2_approximation(depth):
 	i = 1
 	my_frac = 1 + Fraction(1, 2)
@@ -9,4 +10,22 @@ def sqrt_2_approximation(depth):
 		i += 1
 	return my_frac
 
-print sqrt_2_approximation(5)
+def sqrt_3_approximation(depth):
+	i = 1
+	my_frac = 1 + Fraction(1, 1)
+	while i < depth:
+		my_frac = 1 + Fraction(1, 2 - ((i+1)%2) + (my_frac - 1))
+		i += 1
+	return my_frac
+
+def sqrt_23_approximation(depth):
+	i = 1
+	numerator_initial = 4
+	denom_list = [1,3,1,8]
+	my_frac = numerator_initial + Fraction(1, denom_list[0])
+	while i < depth:
+		my_frac = numerator_initial + Fraction (1, denom_list[(i-1)%4] + (my_frac - 4))
+		i += 1
+	return my_frac
+
+print float(sqrt_23_approximation(100)) - math.sqrt(23)
