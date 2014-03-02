66c76e9ddd3e3a0802730ac0489a8d93cb9f90e7
Steven Schmatz <stevenschmatz@gmail.com>
2014-03-01 20:24:38 -0500

    runtime is too high, but it works

--- Python/Pandigital_Products_Attempt_2.py
+++ Python/Pandigital_Products_Attempt_2.py
@@ -1,13 +1,10 @@
 import itertools
 
-count = 0
-for x in itertools.permutations([1,2,3,4,5,6,7,8,9]):
-	count += 1
 
 def is_pandigital(n):
 	length = len(str(n))
 	digit_list = range(10)
-	digit_list = digit_list[:length]
+	digit_list = digit_list[1:length+1]
 	if len(set(list(str(n)))) != length:
 		return False
 	for i in range(length):
@@ -17,5 +14,19 @@ def is_pandigital(n):
 		digit_list.remove(digit)
 	return True
 
-for i in range(count):
-	is_pandigital(1234)
+def pandigital_product_q(a, b):
+	return is_pandigital(int(str(a)+str(b)+str(a*b)))
+
+pan_count = 0
+product_sum = 0
+for a in range(1, 5000):
+	for b in range(a, 5000):
+		if len(set(list(str(a))+list(str(b)))) != len(list(str(a))+list(str(b))):
+			continue
+		if pandigital_product_q(a, b):
+			product_sum += a * b
+
+print product_sum
+
+#for i in range(count):
+#	is_pandigital(12342043)
