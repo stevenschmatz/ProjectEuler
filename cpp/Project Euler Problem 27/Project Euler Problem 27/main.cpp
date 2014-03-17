69a1b382856afa8a6612282825afc2c8d8fc11e0
Steven Schmatz <stevenschmatz@gmail.com>
2014-03-16 22:22:54 -0400

    working solution to problem 27

--- cpp/Project Euler Problem 27/Project Euler Problem 27/main.cpp
+++ cpp/Project Euler Problem 27/Project Euler Problem 27/main.cpp
@@ -2,44 +2,99 @@
 //  main.cpp
 //  Project Euler Problem 27
 //
-//  Created by Schmatz on 3/13/14.
+//  Created by Schmatz on 3/16/14.
 //  Copyright (c) 2014 Schmatz. All rights reserved.
 //
 
-#include <iostream>
-#include <math.h>
-#include <set>
+//  PROMPT:
+//  find a and b in [n**2 + an + b] such that it produces the greatest number of consecutive primes, starting at n = 0. (|a| and |b| < 1000)
+//
+//  IDEAS:
+//  1. Because of n = 0 case, b has to be prime. -> generate all primes from 2 to 1000, about 173 primes.
+//  2. Since [n**2 + an + b] > 1, [n**2 + an] > [1 - b].
+//      Thus, a > [(1 - b - n**2)/n) = [(1 - (b + (n**2))/n]
+//      At the lowest bound at n = 1, a > [(1 - (b + 1))/1] -> a > -b.
+//
+//  Thus, iterate through b primes 2 to 1000, and for each b iterate through a values from -b to 1000.
+//  For each pair (a, b), evaluate expression and break when primality test reveals false.
 
-#define MAX_NUM 10000
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 class Solution {
 public:
-    int primes[MAX_NUM];
     
-    void gen_sieve_primes(void)
+    std::vector<int> generate_primes(int n)
     {
-        for (int p=2; p<MAX_NUM; p++) // for all elements in array
+    std::vector<int> primes;
+    primes.push_back(2);
+    for(int i=3; i < n; i++)
+    {
+        bool prime=true;
+        for(int j=0;j<primes.size() && primes[j]*primes[j] <= i;j++)
         {
-            if(primes[p] == 0) // it is not multiple of any other prime
-                primes[p] = 1; // mark it as prime
-            
-            // mark all multiples of prime selected above as non primes
-            int c=2;
-            int mul = p * c;
-            for(; mul < MAX_NUM;)
+            if(i % primes[j] == 0)
             {
-                primes[mul] = -1;
-                c++;
-                mul = p*c;
+                prime=false;
+                break;
             }
         }
+        if(prime)
+        {
+            primes.push_back(i);
+        }
     }
+    return primes;
+}
     
-} sol;
+    std::vector<int> primes = generate_primes(1000*1000);
 
-int main()
-{
-    sol.gen_sieve_primes();
-    std::cout << sol.primes[32] << std::endl;
+    int number_of_primes(int a, int b) {
+        int primeCount = 0;
+        int n = 0;
+        while (true) {
+            int evaluation = n*n + a*n + b;
+            if (!std::binary_search(primes.begin(), primes.end(), evaluation)) {
+                break;
+            }
+            primeCount++;
+            n++;
+            }
+        return primeCount;
+    };
+    
+    int solve() {
+        int maximumA = 0;
+        int maximumB = 0;
+        int mostPrimes = 0;
+        
+        int a, b;
+        
+        int b_index = 0;
+        while (true) {
+            b = primes[b_index];
+            if (b >= 1000) break;
+            a = -b;
+            while (true) {
+                if (a >= 1000) break;
+                int primeCount = number_of_primes(a, b);
+                if (primeCount > mostPrimes) {
+                    mostPrimes = primeCount;
+                    maximumA = a;
+                    maximumB = b;
+                }
+                a++;
+            }
+            b_index++;
+        }
+        
+        return maximumA*maximumB;
+    }
+
+} solution;
+
+int main() {
+    std::cout << "The product of a and b is " << solution.solve() << '.' << std::endl;
     return 0;
 }
